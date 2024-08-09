a60c3c86e3c42f0f7bfb11f65f9700b37d1362e3
xuhaojun <xuhaojuntw@gmail.com>
2024-08-09 17:14:25 +0800

    test(multiple rent scooter same time): add rent again if rent success (#3)

--- k6/mutilple_rent_same_scooter/script.js
+++ k6/mutilple_rent_same_scooter/script.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
@@ -13,9 +13,11 @@ export default function () {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+  const userId = randomIntFromInterval(1, 100000);
+  const scooterId = randomIntFromInterval(1, 10);
   const body = {
-    userId: randomIntFromInterval(1, 100000),
-    scooterId: randomIntFromInterval(1, 10),
+    userId,
+    scooterId,
   };
   const payload = JSON.stringify(body);
   const params = {
@@ -29,4 +31,11 @@ export default function () {
     'rent success': (r) => r.status === 201,
     'rent failed': (r) => r.status === 400,
   });
+  if (res.status === 201) {
+    sleep(1);
+    const res2 = http.post('http://localhost:3000/rents', payload, params);
+    check(res2, {
+      'rent again failed': (r) => r.status === 400,
+    });
+  }
 }
