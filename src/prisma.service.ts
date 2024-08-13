8c3164f86e8fef4d2009dc67eccae878fd750f94
xuhaojun <xuhaojuntw@gmail.com>
2024-08-13 10:54:03 +0800

    feat(prisma): add log and env PRISMA_LOG_QUERIES

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,6 +3,14 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log:
+        process.env.PRISMA_LOG_QUERIES === 'true'
+          ? ['query', 'info', 'warn', 'error']
+          : [],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
