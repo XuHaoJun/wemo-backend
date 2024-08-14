e397f02c1ab554df98b9a7373324f85fea4ad783
xuhaojun <xuhaojuntw@gmail.com>
2024-08-14 15:34:59 +0800

    feat(log): move to pino for logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
+import { LoggerModule } from 'nestjs-pino';
 import { Module } from '@nestjs/common';
 import { RentModule } from './rent/rent.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { ScooterModule } from './scooter/scooter.module';
 
 @Module({
-  imports: [RentModule, ReservationModule, ScooterModule],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        // install 'pino-pretty' package in order to use the following option
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? { target: 'pino-pretty' }
+            : undefined,
+        // and all the other fields of:
+        // - https://github.com/pinojs/pino-http#api
+        // - https://github.com/pinojs/pino/blob/HEAD/docs/api.md#options-object
+      },
+    }),
+    //
+    RentModule,
+    ReservationModule,
+    ScooterModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
 
   app.useGlobalPipes(new ValidationPipe());
 
